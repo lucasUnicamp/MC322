a0f22823c6cdf6e8a6d13184a7f6f9688af820b9
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-03-11 15:48:45 -0300

    Test methods on Main

--- Lab01/Main.java
+++ Lab01/Main.java
@@ -2,6 +2,16 @@ package Lab01;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Ambiente salaTeste = new Ambiente(100, 100);
+        Robo roboAlfa = new Robo("Alfa", 50, 50);
+        Robo roboBeta = new Robo("Beta", 0, 0);
+
+        roboAlfa.mover(10, -10);
+        salaTeste.dentroDosLimites(roboAlfa.getX(), roboAlfa.getY());
+        roboAlfa.exibirPosicao();
+
+        roboBeta.mover(110, 42);
+        salaTeste.dentroDosLimites(roboBeta.getX(), roboBeta.getY());
+        roboBeta.exibirPosicao();
     }
 }
