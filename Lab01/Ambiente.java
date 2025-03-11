6bc28ba8118ab71cf5b4100d334933be684a0c82
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-03-11 15:28:03 -0300

    Create 'Ambiente' class

---
+++ Lab01/Ambiente.java
@@ -0,0 +1,18 @@
+package Lab01;
+
+public class Ambiente {
+    private int largura;
+    private int altura;
+
+    public Ambiente(int largura, int altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public boolean dentroDosLimites(int x, int y) {
+        if(x < 0 || x > largura || y < 0 || y > altura) 
+            return false;
+        
+        return true;
+    }
+}
