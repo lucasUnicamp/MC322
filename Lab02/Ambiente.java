31fe2c83375589fdcb22a22f8b6520ac065619ba
Leonardo Ferreira <leoferreira04@outlook.com>
2025-03-24 16:46:01 -0300

    muda nome de vaiável no ambiente

--- Lab02/Ambiente.java
+++ Lab02/Ambiente.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 public class Ambiente {
     private int largura;
-    private int altura;
+    private int comprimento;
     private ArrayList<Robo> robosAtivos;
 
     public Ambiente(int largura, int altura) {
         this.largura = largura;
-        this.altura = altura;
+        this.comprimento = altura;
         this.robosAtivos = new ArrayList<>();
     }
 
@@ -18,7 +18,7 @@ public class Ambiente {
     public boolean dentroDosLimites(Robo robo) {
         int x = robo.getX();
         int y = robo.getY();
-        return x >= 0 && x <= largura && y >= 0 && y <= altura;
+        return x >= 0 && x <= largura && y >= 0 && y <= comprimento;
     }
 
     public boolean dentroDosLimites(RoboAereo robo){
@@ -26,6 +26,6 @@ public class Ambiente {
         int y = robo.getY();
         int z = robo.getAltitude();
         int altMax = robo.getAltMax();
-        return (x >= 0 && x <= largura) && (y >= 0 && y <= altura) && (z >= 0 && z <= altMax);
+        return (x >= 0 && x <= largura) && (y >= 0 && y <= comprimento) && (z >= 0 && z <= altMax);
     }
 }
