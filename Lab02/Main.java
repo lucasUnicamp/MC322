82ef002e8f9175b73ff2adeffd3112f1a013ede1
Lucas Henrique Bertanha <l276617@dac.unicamp.br>
2025-03-25 22:35:40 -0300

    Adiciona testes fixos

--- Lab02/Main.java
+++ Lab02/Main.java
@@ -4,16 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Ambiente salaTeste = new Ambiente(100, 100);    // Cria o ambiente para testes
         Scanner scan = new Scanner(System.in);
+        int[][] obstaculos = {
+            {9, 30},
+            {10, 28},
+            {0, 2},
+            {50, 56},
+            {98, 99},
+            {42, 42},
+        };
 
-        Robo roboNormal = new Robo("Alfa", 0, 0, salaTeste); // Cria o robô genérico
-        RoboTerrestre roboTerra = new RoboTerrestre("Beta", 50, 50, 60, salaTeste); // Cria o robô terrestre genérico
+        Robo roboNormal = new Robo("Alfa", 0, 0, salaTeste);    // Cria o robô genérico
+        RoboTerrestre roboTerra = new RoboTerrestre("Beta", 50, 50, salaTeste, 60); // Cria o robô terrestre genérico
         //
         //
-        RoboAereo roboAr = new RoboAereo("Gama", 100, 100, 40, 80, salaTeste); // Cria o robô aéreo genérico
+        RoboAereo roboAr = new RoboAereo("Gama", 100, 100, salaTeste, 40, 80);  // Cria o robô aéreo genérico
         //
         //
-
-
+        
+        salaTeste.adicionarObstaculos(obstaculos);
+        roboNormal.mover(10, 30);
+        roboTerra.mover(65, 97);
 
         scan.close();
     }
