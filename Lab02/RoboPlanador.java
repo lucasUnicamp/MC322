430dc5e2c738c0331cdba1e4241dc5263af2c7ce
Leonardo Ferreira <leoferreira04@outlook.com>
2025-03-25 23:17:28 -0300

    Adição de métodos e propriedades ao roboPlanador

--- Lab02/RoboPlanador.java
+++ Lab02/RoboPlanador.java
@@ -1,7 +1,33 @@
-public class RoboPlanador extends RoboAereo{
-    private int tamanhoAsa; // quanto maior a asa mais plana
+import java.lang.Math;
 
-    public RoboPlanador(String nome, int posicaoX, int posicaoY, int altitude, int altitudeMaxima, Ambiente ambiente){
-        super(nome, posicaoX, posicaoY, altitude, altitudeMaxima, ambiente);
+public class RoboPlanador extends RoboAereo{
+    private int tamanhoAsa; // quanto maior a asa mais consegue planar
+
+    public RoboPlanador(String nome, int posicaoX, int posicaoY, Ambiente ambiente, int altitude, int altitudeMaxima, int tamanhoAsa){
+        super(nome, posicaoX, posicaoY, ambiente, altitude, altitudeMaxima);
+        setTamanhoAsa(tamanhoAsa);
+    }
+
+    @Override
+    public void subir(int metros){
+        if(metros <= tamanhoAsa)
+            super.subir(metros);
+        else
+            System.out.print("Asa muito curta");
+    }
+
+    @Override
+    public void mover(int deltaX, int deltaY){
+        int deslocamento = Math.abs(deltaY) + Math.abs(deltaX);
+        super.mover(deltaX, deltaY);
+        super.descer((150 - tamanhoAsa)*deslocamento); //cai lentamente quando desloca
+    }
+
+    public void setTamanhoAsa(int tamanhoAsa){
+        if (tamanhoAsa <= 100){
+            this.tamanhoAsa = tamanhoAsa;
+        } else {
+            this.tamanhoAsa = 100;
+        }
     }
 }
