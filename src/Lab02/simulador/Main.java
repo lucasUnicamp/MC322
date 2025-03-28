29f8152df551de0132c2d76cb25815ca6cb69ddc
Lucas Henrique Bertanha <l276617@dac.unicamp.br>
2025-03-28 17:26:28 -0300

    Adiciona testes para robô aéreo

--- src/Lab02/simulador/Main.java
+++ src/Lab02/simulador/Main.java
@@ -37,7 +37,7 @@ public class Main {
         roboNormal.mover(-31, -12);     // Teste de 'deltas' negativos que deve falhar devido aos obstáculos
         roboNormal.mover(-5, 10);       // Teste de 'deltas' negativos que deve sucesseder 
 
-        System.out.printf("\n********************************************************************\n");
+        System.out.printf("********************************************************************\n");
         roboTerra.info();      //Inicia testes com robô terrestre
         roboTerra.mover(-15, -25);       // Deve sucesseder
         roboTerra.aumentarVelocidade(70);       // Aumenta a velocidade do robô
@@ -52,6 +52,15 @@ public class Main {
         roboXadrez.mover(0, 2);     // Deve falhar por não estar apontado na direção correta
         roboXadrez.setDirecao("Sul");       // Muda a direção do robô
         roboXadrez.mover(0, -2);        // Teste de 'deltas' negativo que deve sucesseder
+
+        System.out.printf("********************************************************************\n");
+        roboAr.info();      //Inicia testes com robô aéreo
+        roboAr.subir(20);       // Deve sucesseder
+        roboAr.subir(70);       // Deve falhar devido ao limite de altura
+        roboAr.mover(-91, -72);      // Teste para ignorar obstáculos por estar no ar
+        roboAr.descer(10);      // Deve sucesseder
+        roboAr.descer(70);      // Deve falhar por bater no chão 
+        roboAr.mover(1, 2);     // Deve falhar por estar no chão e encontrar um obstáculo
 
         System.out.printf("********************************************************************\n");
         roboPlanador.info();        // Inicia testes com robô aéreo planador
