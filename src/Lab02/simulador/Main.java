746f382b704358fa7545ddfdd3ef1cc9fa31e67b
Lucas Henrique Bertanha <l276617@dac.unicamp.br>
2025-03-28 17:09:43 -0300

    Explica e adiciona mais testes dos robôs

--- src/Lab02/simulador/Main.java
+++ src/Lab02/simulador/Main.java
@@ -25,24 +25,39 @@ public class Main {
         
         salaTeste.adicionarObstaculos(obstaculos);
 
-        roboNormal.mover(10, 30);       // Teste que deve falhar devido aos obstáculos
-        roboNormal.mover(40, 40);       // Teste que deve sucesseder
+        /* 
+         * A seguir encontra-se vários comandos que testam o movimento dos robôs em diversas condições, 
+         * como passar por obstáculos, ser barrado por obstáculos, ir para bordas, tentar fazer movimentos 
+         * não permitidos, etc  
+        */
+        System.out.printf("\n********************************************************************\n");
+        roboNormal.info();      // Inicia testes com robô genérico
+        roboNormal.mover(10, 30);       // Deve falhar devido aos obstáculos
+        roboNormal.mover(40, 40);       // Deve sucesseder
         roboNormal.mover(-31, -12);     // Teste de 'deltas' negativos que deve falhar devido aos obstáculos
         roboNormal.mover(-5, 10);       // Teste de 'deltas' negativos que deve sucesseder 
-        
-        roboPlanador.subir(80);
-        roboPlanador.subir(50);
-        roboPlanador.mover(-30, -10);
-        roboPlanador.mover(10, 40);
 
-        roboXadrez.mover(5, 0);
-        roboXadrez.mover(2, 1);
-        roboXadrez.setTipoMovimento(2);
-        roboXadrez.mover(10, 0);
-        roboXadrez.mover(0, -2);
-        roboXadrez.mover(0, 2);
-        roboXadrez.setDirecao("Sul");
-        roboXadrez.mover(0, -2);
+        System.out.printf("\n********************************************************************\n");
+        roboTerra.info();      //Inicia testes com robô terrestre
+        roboTerra.mover(-15, -25);       // Deve sucesseder
+        roboTerra.aumentarVelocidade(70);       // Aumenta a velocidade do robô
+        roboTerra.mover(50, 50);        // Deve falhar devido ao limite de velocidade
+
+        System.out.printf("********************************************************************\n");
+        roboXadrez.info();      // Inicia testes como robô terrestre xadrez
+        roboXadrez.mover(5, 0);     // Deve falhar por não ser um movimento em L
+        roboXadrez.mover(2, 1);     // Deve sucesseder
+        roboXadrez.setTipoMovimento(2);     // Muda o tipo de movimento para Peão
+        roboXadrez.mover(10, 0);        // Deve falhar por não ser um movimento de 1 ou 2 posições
+        roboXadrez.mover(0, 2);     // Deve falhar por não estar apontado na direção correta
+        roboXadrez.setDirecao("Sul");       // Muda a direção do robô
+        roboXadrez.mover(0, -2);        // Teste de 'deltas' negativo que deve sucesseder
+
+        System.out.printf("********************************************************************\n");
+        roboPlanador.info();        // Inicia testes com robô aéreo planador
+        roboPlanador.subir(80);     // Deve falhar devido ao tamanho das asas
+        roboPlanador.subir(50);     // Deve sucesseder
+        roboPlanador.mover(-30, -10);       // Deve sucesseder
 
         scan.close();
     }
