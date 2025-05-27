61aa1351b540a45377cbf8de3195bf534a3ebef6
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-05-26 23:44:43 -0300

    Acento

--- src/Lab04/cliente/Main.java
+++ src/Lab04/cliente/Main.java
@@ -29,52 +29,34 @@ public class Main {
         /**
          * TESTES MANUAIS
          * 
-         * Essas 4 situacoes devem gerar 4 avisos logo na criaçao dos robos.
-         * O Robo Generico Alfa foi propositalmente colocado dentro de um obstaculo para testar a funcionalidade de recolocar robos em posicoes aleatorias;
-         * O Robo Preguica Delta também foi posto em uma posicao invalida, fora do Ambiente, para o mesmo tipo de teste;
-         * O Robo Satelite Sigma foi propositalemente posto numa altura menor do que a de orbita para testar se caia no chao;
-         * 2 sensores iguais foram postos no Robo Generico Alfa para testar funcionalidade de rejeicao a sensores repetidos;
-         * 
-         * Acreditamos que esses sao os unicos 'casos' que podem ser testados que nao dependem de entradas de usuario (assim como foi pedido nas instruçoes do lab), 
-         * ja que dependem de onde instanciamos os robos. Outros acontecimentos, como colidir com obstaculo, nao ter carga suficiente, monitorar uma posicao, etc,
-         * podem todos serem feitos facilmente pelo menu interativo com poucas entradas. 
          */
         System.out.print("\n## CRIACAO ROBOS ################################");
-        // Instanciamento em massa de robos e adicao de sensores por composiçao (sensor eh instanciado dentro do robo, logo nao existe fora dele)
-        RoboTerrestre roboTerrestre = new RoboTerrestre("Beta", "RT01", 25, 25, salaTeste, 60);     // Cria o robo terrestre generico
+        // Instanciamento em massa de robôs e adição de sensores por composição (sensor eh instanciado dentro do robô, logo não existe fora dele)
+        RoboTerrestre roboTerrestre = new RoboTerrestre("Beta", "RT01", 25, 25, salaTeste, 60);     // Cria o robô terrestre genérico
         roboTerrestre.adicionarSensor(new SensorObstaculo(10, salaTeste));
         roboTerrestre.adicionarSensor(new SensorObstaculo(30, salaTeste));
         roboTerrestre.getDescricao();
-        RoboXadrez roboXadrez = new RoboXadrez("Theta", "RT02", 40, 20, salaTeste, 6, 1);       // Cria o robo terrestre do tipo xadrez
+        RoboXadrez roboXadrez = new RoboXadrez("Theta", "RT02", 40, 20, salaTeste, 6, 1);       // Cria o robô terrestre do tipo xadrez
         roboXadrez.adicionarSensor(new SensorObstaculo(5, salaTeste));
         roboXadrez.getDescricao();
-        RoboPreguica roboPreguica = new RoboPreguica("Delta", "RT03", 30, 42, salaTeste, 25, 1);        // Cria o robo terrestre do tipo preguiça
+        RoboPreguica roboPreguica = new RoboPreguica("Delta", "RT03", 30, 42, salaTeste, 25, 1);        // Cria o robô terrestre do tipo preguiça
         roboPreguica.adicionarSensor(new SensorTemperatura(20, salaTeste));
         roboPreguica.getDescricao();
-        RoboAereo roboAereo = new RoboAereo("Gama", "RA01", 5, 5, salaTeste, 40, 80);      // Cria o robo aereo generico
+        RoboAereo roboAereo = new RoboAereo("Gama", "RA01", 5, 5, salaTeste, 40, 80);      // Cria o robô aéreo genérico
         roboAereo.adicionarSensor(new SensorObstaculo(40, salaTeste));
         roboAereo.adicionarSensor(new SensorTemperatura(10, salaTeste));
-        RoboPlanador roboPlanador = new RoboPlanador("Phi", "RA02", 5, 40, salaTeste, 0, 80, 50);      // Cria o robo aereo do tipo planador
+        RoboPlanador roboPlanador = new RoboPlanador("Phi", "RA02", 5, 40, salaTeste, 0, 80, 50);      // Cria o robô aéreo do tipo planador
         roboPlanador.adicionarSensor(new SensorObstaculo(20, salaTeste));
         roboPlanador.getDescricao();
-        RoboSatelite roboSatelite = new RoboSatelite("Sigma", "RA03", 30, 40, salaTeste, 10, 50, 30, 0);        // Cria o robo aereo do tipo satelite
+        RoboSatelite roboSatelite = new RoboSatelite("Sigma", "RA03", 30, 40, salaTeste, 10, 50, 30, 0);        // Cria o robô aéreo do tipo satélite
         roboSatelite.adicionarSensor(new SensorObstaculo(50, salaTeste));
         roboSatelite.adicionarSensor(new SensorTemperatura(50, salaTeste));
         roboSatelite.getDescricao();
 
-        System.out.println("");
-        try {
-            roboSatelite.enviarMensagem(roboXadrez, "Mensagem teste 1");
-            roboXadrez.enviarMensagem(roboSatelite, "Mensagem teste 2");
-            salaTeste.getCentral().exibirMensagens();
-        } catch (RoboDesligadoException erro) {
-            System.out.println(erro.getMessage());
-        }
-
         /**
          * TESTES INTERATIVOS
          * 
-         * Aqui o usuario pode fazer quantos testes quiser rapidamente, uma vez que açoes podem ser feitas uma logo apos a outra.
+         * Aqui o usuário pode fazer quantos testes quiser rapidamente, uma vez que ações podem ser feitas uma logo após a outra.
          */
         Menu menu = new Menu(salaTeste, scan);
         menu.iniciarMenuPrincipal();
